227f10abc6be938429c23a2936820ac2b96e0b2b
kycnexynpuwejl <kycnexynpuwejl@protonmail.com>
2022-06-07 13:34:52 +0300

    RabbitMQ.Client nuget package demo project

---
+++ Producer/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory
+{
+    Uri = new Uri("amqp://guest:guest@localhost:5672")
+};
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+channel.QueueDeclare("demo-queue",durable: true, exclusive: false, autoDelete: false, arguments: null);
+
+var message = new {Name = "Producer", Message = "Hello!"};
+byte[] body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message));
+
+channel.BasicPublish("", "demo-queue", null, body);
